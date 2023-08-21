8bcea4976a3e23a347aa1b8bfbb43ebbd9588a05
Bassanteltaweel99 <passantzakaria0@gmail.com>
2023-08-21 03:36:11 -0700

    task

--- main.c
+++ main.c
@@ -14,6 +14,7 @@ int main(void)
     unsigned int ui;
     void *addr;
 
+    int _printf(const char *format, ...);
     len = _printf("Let's try to printf a simple sentence.\n");
     len2 = printf("Let's try to printf a simple sentence.\n");
     ui = (unsigned int)INT_MAX + 1024;
